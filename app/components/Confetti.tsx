cb5efb0d70c37e9afef146e7b2494fb99fc62b17
kozmoss <sametbuzcu@outlook.com>
2024-05-21 00:42:46 +0300

    bug fix

--- app/components/Confetti.tsx
+++ app/components/Confetti.tsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
+
 const SHAPES = ["square", "triangle"];
 const COLOR_DIGIT = "ABCDEF1234567890";
 
-const Confetti = ({isConfettiActive}: {isConfettiActive: boolean}) => {
-  const containerRef = useRef<any>(null);
+const Confetti = ({ isConfettiActive }: { isConfettiActive: boolean }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (isConfettiActive) {
       generateConfetti();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isConfettiActive]);
 
   const generateRandomColor = () => {
@@ -27,24 +29,30 @@ const Confetti = ({isConfettiActive}: {isConfettiActive: boolean}) => {
         const positionX = Math.random() * window.innerWidth;
         const positionY = Math.random() * window.innerHeight;
         const rotation = Math.random() * 360;
-        const size = Math.floor(Math.random() * (20 - 5 + 1)) + 5; // Set confetti styles
+        const size = Math.floor(Math.random() * (20 - 5 + 1)) + 5;
+
+        // Set confetti styles
+        confetti.style.position = 'absolute';
         confetti.style.left = `${positionX}px`;
         confetti.style.top = `${positionY}px`;
         confetti.style.transform = `rotate(${rotation}deg)`;
-        confetti.className =
-          "confetti " + SHAPES[Math.floor(Math.random() * 3)];
+        confetti.className = "confetti " + SHAPES[Math.floor(Math.random() * SHAPES.length)];
         confetti.style.width = `${size}px`;
         confetti.style.height = `${size}px`;
-        confetti.style.backgroundColor = generateRandomColor(); // Append confetti to the container
+        confetti.style.backgroundColor = generateRandomColor();
+
+        // Append confetti to the container
         container.appendChild(confetti);
+
         // Remove confetti element after animation duration (4 seconds)
         setTimeout(() => {
-          container.removeChild(confetti);
+          if (container.contains(confetti)) {
+            container.removeChild(confetti);
+          }
         }, 4000);
       }
     }
   };
-
 
   return (
     <div>
